0da2f59582574096f2a78e8705baf5172508598f
Ailko <ailko.claeys@outlook.com>
2020-02-21 11:31:29 +0100

    Heldproperties toegevoegd

--- src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace src_HeroopQuest.GameClasses
 {
+    enum HeldType { Barbaar, Dwerg, Elf, Tovenaar};
     class Held
     {
+        public string Naam { get; set; }
+        public string Beschrijving { get; private set; }
+        public int MaxIntelligentie { get; private set; }
+        public int MaxLichaam { get; private set; }
+        public int HuidigIntelligentie { get; set; }
+        public int HuidigLichaam { get; set; }
+        public HeldType HeldType { get; private set; }
+        public int AanvalsDobbelstenen { get; private set; } = 2;
+        public int VerdedegingsDobbelstenen { get; private set; } = 2;
+        public int LoopDobbelstenen { get; private set; } = 2;
     }
 }
