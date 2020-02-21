b8b25fac316d471a23c8ac1de2c4aa800d861074
Ailko <ailko.claeys@outlook.com>
2020-02-21 11:53:27 +0100

    Constructor Held Toegevoegd

--- src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
@@ -17,5 +17,37 @@ namespace src_HeroopQuest.GameClasses
         public int AanvalsDobbelstenen { get; private set; } = 2;
         public int VerdedegingsDobbelstenen { get; private set; } = 2;
         public int LoopDobbelstenen { get; private set; } = 2;
+
+        public Held(HeldType type, string naam)
+        {
+            Naam = naam;
+            HeldType = type;
+
+            switch (type)
+            {
+                case HeldType.Barbaar:
+                    MaxIntelligentie = 2;
+                    MaxLichaam = 8;
+                    AanvalsDobbelstenen = 3;
+                    break;
+                case HeldType.Dwerg:
+                    MaxIntelligentie = 3;
+                    MaxLichaam = 7;
+                    break;
+                case HeldType.Elf:
+                    MaxIntelligentie = 4;
+                    MaxLichaam = 6;
+                    break;
+                case HeldType.Tovenaar:
+                    MaxIntelligentie = 6;
+                    MaxLichaam = 4;
+                    AanvalsDobbelstenen = 1;
+                    break;
+                default:
+                    break;
+            }
+            HuidigIntelligentie = MaxIntelligentie;
+            HuidigLichaam = MaxLichaam;
+        }
     }
 }
