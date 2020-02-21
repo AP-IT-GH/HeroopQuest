d894741bbe271ef2e761e2a7a1404410564dabd4
Tim Dams <tim.dams@ap.be>
2020-02-21 11:54:13 +0100

    ik heb helden!

--- src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
@@ -7,6 +7,37 @@ namespace src_HeroopQuest.GameClasses
     enum HeldType {Barbaar, Dwerg, Elf, Tovenaar }
     class Held
     {
+        public Held(HeldType type, string naam)
+        {
+            Naam = naam;
+            HeldType = type;
+            switch (type)
+            {
+                case HeldType.Barbaar:
+                    AanvalsDobbelstenen = 3;
+                    MaxIntelligentie = 2;
+                    MaxLichaam = 8;
+                    break;
+                case HeldType.Dwerg:
+                    MaxIntelligentie = 3;
+                    MaxLichaam = 7;
+                    break;
+                case HeldType.Elf:
+                    MaxIntelligentie = 4;
+                    MaxLichaam = 6;
+                    break;
+                case HeldType.Tovenaar:
+                    AanvalsDobbelstenen = 1;
+                    MaxIntelligentie = 6;
+                    MaxLichaam = 4;
+                    break;
+                default:
+                    break;
+            }
+            HuidigIntelligentie = MaxIntelligentie;
+            HuidigLichaam = MaxLichaam;
+        }
+
         public string Naam { get; set; }
         public string Beschrijving { get; private set; }
         public int MaxIntelligentie { get;private set; }
--- src_HeroopQuest/src_HeroopQuest/Program.cs
+++ src_HeroopQuest/src_HeroopQuest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using src_HeroopQuest.GameClasses;
+using System;
 
 namespace src_HeroopQuest
 {
@@ -6,7 +7,11 @@ namespace src_HeroopQuest
     {
         static void Main(string[] args)
         {
-         
+            Held held1 = new Held(HeldType.Barbaar, "Conan");
+            Held held2 = new Held(HeldType.Tovenaar, "Bart");
+
+            Console.WriteLine(held1.AanvalsDobbelstenen);
+            Console.WriteLine(held2.HeldType);
         }
     }
 }
