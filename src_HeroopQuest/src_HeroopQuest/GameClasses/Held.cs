3f9bcef93591fc9986c699f53bf74184f7073f94
Tim Dams <tim.dams@ap.be>
2020-03-04 11:54:00 +0100

    einde les 2,(helden hebben List, resource en foreach)

--- src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using src_HeroopQuest.Properties;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -14,19 +15,23 @@ namespace src_HeroopQuest.GameClasses
             switch (type)
             {
                 case HeldType.Barbaar:
+                    Beschrijving = Resources.barbaarTekst;
                     AanvalsDobbelstenen = 3;
                     MaxIntelligentie = 2;
                     MaxLichaam = 8;
                     break;
                 case HeldType.Dwerg:
+                    Beschrijving = Resources.dwergTekst;
                     MaxIntelligentie = 3;
                     MaxLichaam = 7;
                     break;
                 case HeldType.Elf:
+                    Beschrijving = Resources.elfTekst;
                     MaxIntelligentie = 4;
                     MaxLichaam = 6;
                     break;
                 case HeldType.Tovenaar:
+                    Beschrijving = Resources.tovenaarTekst;
                     AanvalsDobbelstenen = 1;
                     MaxIntelligentie = 6;
                     MaxLichaam = 4;
@@ -49,5 +54,19 @@ namespace src_HeroopQuest.GameClasses
         public int AanvalsDobbelstenen { get; private set; } = 2;
         public int VerdigingsDobbelstenen { get; private set; } = 2;
         public int LoopDobbelstenen { get; private set; } = 2;
+
+        public void ToonFiche()
+        {
+          
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\t\t~~~~{Naam} de {HeldType}~~~~");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(Beschrijving);
+
+            Console.WriteLine($"\tLichaamspunten: \t{HuidigLichaam}/{MaxLichaam}");
+            Console.WriteLine($"\tIntelligentie: \t\t{HuidigIntelligentie}/{MaxIntelligentie}");
+            Console.ResetColor();
+
+        }
     }
 }
--- src_HeroopQuest/src_HeroopQuest/Program.cs
+++ src_HeroopQuest/src_HeroopQuest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using src_HeroopQuest.GameClasses;
+using src_HeroopQuest.Properties;
 using System;
+using System.Collections.Generic;
 
 namespace src_HeroopQuest
 {
@@ -7,11 +9,38 @@ namespace src_HeroopQuest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Resources.barbaarTekst);
+
             Held held1 = new Held(HeldType.Barbaar, "Conan");
             Held held2 = new Held(HeldType.Tovenaar, "Bart");
+            Held held3 = new Held(HeldType.Dwerg, "Gimli");
 
-            Console.WriteLine(held1.AanvalsDobbelstenen);
-            Console.WriteLine(held2.HeldType);
+            held1.ToonFiche();
+            held2.ToonFiche();
+
+            List<Held> deHelden = new List<Held>();
+            deHelden.Add(held1);
+            deHelden.Add(held2);
+
+            deHelden.Insert(0, held3);
+            deHelden.RemoveAt(1);
+            Console.Clear();
+
+
+            foreach (var heldje in deHelden)
+            {
+                heldje.ToonFiche();
+            }
+
+            for (int i = 0; i < deHelden.Count; i++)
+            {
+                deHelden[i].ToonFiche();
+            }
+
+
+
+            //static
+            //heldlijst creeren en als resultaat teruggeven 
         }
     }
 }
